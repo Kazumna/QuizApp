e11e766b86691f79ac8cbf7f3e216ae84216031c
Ady <ady@dnsspecialistservices.com.au>
2023-09-15 08:38:51 +1000

    Really good explanation of usage of Map and Looping

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   //To use in build method, must add extra properties to this class
   final List<String> chosenAnswers;
 
-  //if you wanna receive data in a widget, must accept argument in a constructor function
+  //All values in dart are Object which are flexible type and accept all kinds of value.
+  List<Map<String, Object>> getSummaryData() {
+    //the type should be the same
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0 ; i < chosenAnswers.length; i++) {
+      //wanna store a collection of pieces of data such as MAP, key value pairs
+      summary.add({
+        //can add any Key of your choice as long as its String type
+        'question_index': i,
+        //question text
+        'question': questions[i].text,
+        //The first answer is always correct one
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
+
+  //if you wanna receive data in a widget, must accept argument in a constructor function.
   const ResultsScreen({
     super.key,
     required this.chosenAnswers,
